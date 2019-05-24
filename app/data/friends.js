122a39fed565e02fae878a104be42911a324b768
Wes McC <wkmccowin@yahoo.com>
2019-05-24 11:48:36 -0700

    Got through routing logic, having some issues getting connected however. Going to proceed with creating score arrays for each friend object.

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+var friendsarray = [
+    {
+    "name":"Jenn",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+    
+    {
+        "name":"Wanda",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            3,
+            1,
+            2
+          ]
+
+    },
+
+    {
+        "name":"Lila",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            1,
+            4,
+            1,
+            2,
+            1,
+            5,
+            3,
+            5,
+            2
+          ]
+
+    },
+
+    {
+        "name":"Lauren",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            4,
+            1,
+            3,
+            2,
+            5,
+            1,
+            2,
+            1,
+            3
+          ]
+
+    }
+];
+
+module.exports = friendsarray;
--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+var path =require('path');
 
 const app = express();
 var PORT = process.env.PORT||8080;
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
  
-app.listen(3000)
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
  
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require('./app/routing/htmlRoutes.js')(app);
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT" + PORT);
